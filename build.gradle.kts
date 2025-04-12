5f0197d6641e19d61e81a2e8e3b4bd0e6bd7b54a
zhufucdev <zhufuzhufu1@gmail.com>
2025-04-12 09:44:45 +0800

    fix: version 0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -149,7 +149,7 @@ mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
 
-    coordinates(Library.namespace, "core", "0.1.0")
+    coordinates(Library.namespace, "core", "0.0.1")
     pom {
         name = "Hugging Face Tokenizers KMP"
         description = "Kotlin binding to the Hugging Face tokenizers, as a Multiplatform library."
